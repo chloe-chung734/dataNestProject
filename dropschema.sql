2f1183f145045813a2ec2fceac340dc074a92ff2
nagyeop <4rldur0@gmail.com>
2026-06-09 14:03:36 +0900

    feat: add views and update schema scripts

--- dropschema.sql
+++ dropschema.sql
@@ -1,3 +1,8 @@
+DROP VIEW IF EXISTS ewha.order_details_view;
+DROP VIEW IF EXISTS ewha.customer_spending_view;
+DROP VIEW IF EXISTS ewha.v_item_sales_by_price_era;
+DROP VIEW IF EXISTS ewha.v_customer_sales_by_demo;
+
 DROP TABLE IF EXISTS ewha.bill;
 DROP TABLE IF EXISTS ewha.order_item;
 DROP TABLE IF EXISTS ewha.orders;
